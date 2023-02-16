96e9fdea4efdfe4043032d59287173b074fed2d9
Lovely123Mastery <106240801+Lovely123Mastery@users.noreply.github.com>
2023-02-16 11:33:55 +0530

    commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const productRouter = require('./Routes/productRoutes');
+const productRouter = require("./Routes/productRoutes");
 
 app.use(express.json());
 
- app.use('/api/v1/products', productRouter);
+app.use("/api/v1/products", productRouter);
 module.exports = app;
+//commit
